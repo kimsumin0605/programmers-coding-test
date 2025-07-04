7fbb7a20c251799933ec922b824ec23b275adcca
sumin <suminon@naver.com>
2025-07-05 00:28:27 +0900

    대소문자 바꿔서 출력하기

---
+++ Programmers/2_week/대소문자바꿔서출력하기.js
@@ -0,0 +1,31 @@
+/**
+ * 
+ * 대소문자 바꿔서 출력하기
+ * 
+ * 영어 알파벳으로 이루어진 문자열 str이 주어집니다.
+ * 각 알파벳을 대문자는 소문자로 소문자는 대문자로 변환해서 출력하는 코드를 작성해 보세요.
+ * 
+ * 제한사항
+ * 1 ≤ str의 길이 ≤ 10
+ * str은 알파벳으로만 이루어진 문자열입니다.
+ */
+
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = [line];
+}).on('close',function(){
+    str = input[0];
+    
+    let reverse = '';
+    for (let r of str){
+        reverse += r === r.toLowerCase()?r.toUpperCase():r.toLowerCase();
+    }
+    console.log(reverse)
+});
