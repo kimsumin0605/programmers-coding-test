a48e9c76a133f885ae6782d75b79b65d6b49e065
sumin <suminon@naver.com>
2025-07-07 01:57:51 +0900

    ts 버전 추가

--- Programmers/1_week/택배상자꺼내기.js
+++ Programmers/1_week/택배상자꺼내기.js
@@ -14,3 +14,24 @@ function solution(n, w, num) {
   
   return count;
 }
+/** 타입스크립트 마이그레이션
+ 
+function solution(n: number, w: number, num: number): number {
+  const row: number = Math.floor((num - 1) / w);
+  const pos: number = (num - 1) % w;
+  const col: number = row % 2 === 0 ? pos : w - 1 - pos;
+  const totalRows: number = Math.ceil(n / w);
+
+  let count = 0;
+
+  for (let r = row; r < totalRows; r++) {
+    const start = r * w + 1;
+    const idx = r % 2 === 0 ? col : w - 1 - col;
+    const boxNum = start + idx;
+
+    if (boxNum <= n) count++;
+  }
+
+  return count;
+}
+  */
