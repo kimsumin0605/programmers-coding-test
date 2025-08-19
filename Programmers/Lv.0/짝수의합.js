dab421f9af9ce02bfcd2e31ede4263f7f7548670
sumin <suminon@naver.com>
2025-08-19 11:15:22 +0900

    짝수의 합

---
+++ Programmers/Lv.0/짝수의합.js
@@ -0,0 +1,12 @@
+/**
+ * 문제 설명
+ * 정수 n이 주어질 때, n이하의 짝수를 모두 더한 값을 return 하도록 solution 함수를 작성해주세요.
+ */
+
+function solution(n) {
+    let sum = 0;
+    for(let i = 0; i <= n; i += 2 ){
+        sum += i; 
+    }
+    return sum;
+}
