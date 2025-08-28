ff7b2b29a753a001467e070f622a0cc37692d3d4
sumin <suminon@naver.com>
2025-08-28 09:31:52 +0900

    짝수는 싫어요

---
+++ Programmers/Lv.0/짝수는싫어요.js
@@ -0,0 +1,15 @@
+/**
+ * 문제 설명
+ * 정수 n이 매개변수로 주어질 때, 
+ * n 이하의 홀수가 오름차순으로 담긴 배열을 return하도록 solution 함수를 완성해주세요.
+ */
+
+function solution(n) {
+  let arr = [];
+  for (let i = 0; i <= n; i++) {
+    if (i % 2 !== 0) {
+      arr.push(i);
+    }
+  }
+  return arr.sort((a, b) => a - b);
+}
